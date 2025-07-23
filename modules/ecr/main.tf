61288fab17f34a300553d1e44d172a550da3307b
saren-dex <sarenkumar.p@icliniq.com>
2025-07-23 23:25:17 +0530

    Update ECR repository names in CI/CD workflow and add lifecycle policy for image expiration

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -2,7 +2,8 @@ resource "aws_ecr_repository" "repo" {
   name                 = "${var.project_name}-${var.environment}-${var.name_suffix}"
   image_tag_mutability = "MUTABLE"
 
-  force_delete = var.force_delete 
+  force_delete = var.force_delete
+
   image_scanning_configuration {
     scan_on_push = true
   }
@@ -14,4 +15,28 @@ resource "aws_ecr_repository" "repo" {
   tags = merge({
     Name = "${var.project_name}-${var.environment}-${var.name_suffix}-ecr"
   }, var.tags)
+}
+
+resource "aws_ecr_lifecycle_policy" "lifecycle" {
+  repository = aws_ecr_repository.repo.name
+
+  policy = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire all images older than 5 days",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
 }
